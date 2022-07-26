b198845121e4a7714bf1defd68237e504bbf0f27
Sanjit-Sahoo <109169913+Sanjit-Sahoo@users.noreply.github.com>
2022-07-26 14:24:36 -0400

    Create PIRMotionLEDBuzzer.ino

---
+++ PIRMotionLEDBuzzer/PIRMotionLEDBuzzer.ino
@@ -0,0 +1,40 @@
+int led = 13;
+int sensor = 8;
+int buzzer = 12;
+int state = LOW;
+int val = 0;
+
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(sensor, INPUT);
+  pinMode(buzzer, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  val = digitalRead(sensor);
+  if (val == HIGH) {
+    digitalWrite(9, LOW);
+    digitalWrite(11, HIGH);
+    digitalWrite(12, HIGH);
+    tone(buzzer, 1000);
+    delay(3000);
+    
+    if (state == LOW) {
+      Serial.println("Motion detected!"); 
+      state = HIGH;
+    }
+  } 
+  else {
+      digitalWrite(11, LOW);
+      digitalWrite(9, HIGH);
+      digitalWrite(12, LOW);
+      digitalWrite(12, LOW);
+      delay(20);
+
+      if (state == HIGH){
+        Serial.println("Motion stopped!");
+        state = LOW;
+    }
+  }
+}
